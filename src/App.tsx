5313dac4608aadd039e4a6681a9cfc77040ac039
NaveenPrajapati <solidauto202@gmail.com>
2023-06-04 18:49:05 +0530

    do some finishing

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import List from "./comp/List";
 
 function App() {
   const [list, setList] = useState<string[]>([]);
-  const [edval,setEdVal]=useState<string | number>('');
-  const [txt,setTxt]=useState('');
-let edit='';
   function save(event:React.KeyboardEvent<HTMLInputElement>) {
     const value = event.currentTarget.value;
   if (event.key === "Enter" && value !== "") {
@@ -19,20 +16,21 @@ let edit='';
 
   return (
     <div className="w-full h-[100vh]">
-      <div className="w-[400px] shadow-2xl p-5 mx-auto mt-[200px] min-h-[500px] relative">
+      <div className="max-w-[400px] shadow-2xl p-5 mx-auto mt-[200px] h-[500px] overflow-y-auto relative">
         <input
           type="text"
           onKeyDown={save}
           placeholder="enter text" 
-          className="w-full mb-2 bg-slate-100 p-1 rounded-md"
-          
-      
+          className="w-full mb-2 bg-slate-400 p-1 text-white rounded-md font-semibold"
         ></input>
+  
+
         <List list={list} setList={setList}/>
 
+{/* 
         <button className="bg-red-400 py-1 px-2 rounded-md absolute right-0 bottom-0 mr-1 mb-1">
           Delete Marked
-        </button>
+        </button> */}
       </div>
     </div>
   );
--- src/comp/List.tsx
+++ src/comp/List.tsx
@@ -15,7 +15,6 @@ const[ind,setInd]=useState<number>(0)
 const[edit,setEdit]=useState(false)
 
 const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-  console.log(event.target.value)
   setChange(event.currentTarget.value);
 };
 
@@ -34,6 +33,7 @@ const handleKeyboard: React.KeyboardEventHandler<HTMLInputElement> = (event) => 
 
 
   function deleteVal( key: number) {
+    setEdit(false)
     let newArr = [...list];
     newArr.splice(key, 1);
     setList(newArr);
@@ -49,14 +49,14 @@ setEdit(true)
   }
 
   return (
-    <div>
+    <div className="">
 
     {list?.map((li,index)=>(
       <div key={index} className="w-full border-b-blace-1 mt-2 p-1 flex justify-between items-center shadow-xl bg-slate-100">
       <div className="flex items-center gap-1">
         <input type="checkbox" id="check" />
-        {!(edit && ind===index ) && <p>{li}</p> }
-     {(edit && ind===index ) && <input type="text" name="textValue" value={change} onChange={handleChange}  onKeyDown={handleKeyboard}/>}
+        {!(edit && ind===index ) && <p className="font-semibold text-slate-600">{li}</p> }
+     {(edit && ind===index ) && <input type="text" name="textValue" value={change} className="px-1 w-full rounded-md outline-none font-semibold text-slate-600" onChange={handleChange}  onKeyDown={handleKeyboard}/>}
       </div>
       <div className="flex gap-2">
       <button onClick={()=>editVal(index,li)}>
